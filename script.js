d011a39e5b6dea9270f2c45eea462cd5fe85e416
Willdooo <72411792+Willdooo@users.noreply.github.com>
2021-02-25 10:43:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 // const gameBoard - create board, clear board, update board
 // const displayController - check for win, rounds
-// const gamePlayer - factory =sign
+// const gamePlayer - factory =sign + playercreation
 // const
+
+//module
 const gameBoard = (() =>{
     //this stores either X or Y
-    const boardPlayerValue = ["", "", "", "", "", "", "", "", "",]
+    let boardPlayerValue = []
+    for( let i = 0; i < 9; i++){
+        boardPlayerValue.push("")
+    }
     //this function stores sign of player in boardPlayerValue
     const inBoard = (position, sign) => {
         if (position > board.length) return
         boardPlayerValue[position] = sign
     }
-    //this exports position of sign
-    const outBoard = (position) =>{
-        if (position > board.length) return
-        return boardPlayerValue[position]
-    }
+
+
     const startB = document.querySelector("#startGame")
     startB.addEventListener("click", createBoard)
     function createBoard(){
         const buildingBoard = document.querySelector("#gridContainer")
-        for (let i = 1; i < 10; i++){
+        for (let i = 0; i < 9; i++){
             let grids = document.createElement("div")
             buildingBoard.insertAdjacentElement("afterbegin", grids)
             grids.setAttribute("data-number", `${i}`)
@@ -28,6 +30,18 @@ const gameBoard = (() =>{
       let wholeBoard = buildingBoard.querySelectorAll("div")
       wholeBoard.forEach(block => block.addEventListener("click", updateBoard))
         };
+    
+  /*  const clearB = document.querySelector("#clearGrid")
+    clearB.addEventListener("click", clearBoard)
+      function clearBoard(){
+        const buildingBoard = document.querySelector("#gridContainer")
+        let wholeBoard = buildingBoard.querySelectorAll("div")
+        wholeBoard.forEach(block => block.textContent = "")
+        for( let i = 0; i < boardPlayerValue.length; i++){
+            boardPlayerValue[i] = ""
+        }
+        displayController.resetRoundC()
+    }*/
 
     function updateBoard(){
         if(this.textContent == "X" || this.textContent == "O"){
@@ -38,19 +52,19 @@ const gameBoard = (() =>{
    }
 
     //TODO - clear board
-    return {createBoard, inBoard, outBoard}
+    return {createBoard, /*clearBoard*/ inBoard,}
 })();
 
-
+//factory
 const gamePlayer = (sign) =>{
-    this.sign = sign //this will be private
+    this.sign = sign //
     const showSign = () =>{
-        return sign
+       return sign
     }
     return {showSign} //public
 };
 
-
+//module
 const displayController = (() =>{
     //create players
     const playerX = gamePlayer("X")
@@ -66,11 +80,26 @@ const displayController = (() =>{
         }else if(roundC % 2 ===0){
             roundC++
             return playerO.showSign()
+        }
     }
+    const resetRoundC = () =>{
+        let roundC = 1;
+        return roundC
     }
-    //input board
-
     //check for win --> win conditions
-    return{whichSign}
+    const conditionsForWin = [
+        [0, 1, 2],
+        [0, 4, 8],
+        [0, 3, 6],
+        [3, 4, 5],
+        [6, 4, 2],
+        [6, 7, 8],
+        [1, 4, 7],
+        [2, 5, 8]
+    ]
+    const checkForWin = () =>{
+
+    }
+    return{whichSign, resetRoundC}
 })();
 
