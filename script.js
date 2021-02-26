eb02ad92c45861c128d906d47a801c36da3e0fb1
Willdooo <72411792+Willdooo@users.noreply.github.com>
2021-02-26 09:27:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,16 +16,13 @@ const gameBoard = (() =>{
         if (position > boardPlayerValue.length) return
         boardPlayerValue[position] = sign
     } 
-    const outBoard = (position) => {
-        if(position > boardPlayerValue.length) return
-        return boardPlayerValue[position]
-    }
 
 
     const startB = document.querySelector("#startGame")
     startB.addEventListener("click", createBoard)
+    const buildingBoard = document.querySelector("#gridContainer")
     function createBoard(){
-        const buildingBoard = document.querySelector("#gridContainer")
+
         for (let i = 0; i < 9; i++){
             let grids = document.createElement("div")
             buildingBoard.insertAdjacentElement("afterbegin", grids)
@@ -35,14 +32,20 @@ const gameBoard = (() =>{
       let wholeBoard = buildingBoard.querySelectorAll("div")
       wholeBoard.forEach(block => block.addEventListener("click", updateBoard))
       wholeBoard.forEach(block => block.addEventListener("click", storePosition))
+      wholeBoard.forEach(block => block.addEventListener("click", alerWin))
+
     };
     function storePosition () {
         inBoard(this.dataset.number, this.textContent)
         console.log(boardPlayerValue)
      
     }
-    
-  /* const clearB = document.querySelector("#clearGrid")
+    function alerWin(){
+        displayController.checkForWin(boardPlayerValue)
+    }
+ 
+
+    const clearB = document.querySelector("#clearGrid")
     clearB.addEventListener("click", clearBoard)
       function clearBoard(){
         const buildingBoard = document.querySelector("#gridContainer")
@@ -52,7 +55,16 @@ const gameBoard = (() =>{
             boardPlayerValue[i] = ""
         }
         displayController.resetRoundC()
-    }*/
+        const winMess = document.querySelector("#winnerDiv")
+        winMess.textContent = "Who is going to win?"
+     //   wholeBoard.forEach(block => block.addEventListener("click", updateBoard))
+    }
+    const stopGame = () =>{
+        let wholeBoard = buildingBoard.querySelectorAll("div")
+        wholeBoard.forEach(block => block.removeEventListener("click", updateBoard))
+        clearBoard()
+
+    }
 
     function updateBoard(){
         if(this.textContent == "X" || this.textContent == "O"){
@@ -66,7 +78,7 @@ const gameBoard = (() =>{
          console.log(boardPlayerValue)
     }
     //TODO - clear board
-    return {createBoard, /*clearBoard*/ inBoard, outBoard, showBoardStatus,}
+    return {createBoard, clearBoard, inBoard, showBoardStatus, alerWin, stopGame  }
 })();
 
 //factory
@@ -99,29 +111,63 @@ const displayController = (() =>{
         }
     }
 
-  /*  const resetRoundC = () =>{
-        let roundC = 1;
+    const resetRoundC = () =>{
+        roundC = 1;
         return roundC
-    }*/
-    //check for win --> win conditions
-    const conditionsForWin = [
-
-        // 0 1 2 
-        // 3 4 5
-        // 6 7 8
-
-        [0, 1, 2],
-        [0, 4, 8],
-        [0, 3, 6],
-        [3, 4, 5],
-        [6, 4, 2],
-        [6, 7, 8],
-        [1, 4, 7],
-        [2, 5, 8]
-    ]
-    const checkForWin = () =>{
-
     }
-    return{whichSign,  /*resetRoundC*/}
+    //check for win --> win conditions
+    const checkForWin = (boardArray) =>{
+        winnerX = false;
+        winnerO = false;
+        draw = false;
+        const conditionsForWin = [
+            // 0 1 2 
+            // 3 4 5
+            // 6 7 8
+            [0, 1, 2],
+            [0, 4, 8],
+            [0, 3, 6],
+            [3, 4, 5],
+            [6, 4, 2],
+            [6, 7, 8],
+            [1, 4, 7],
+            [2, 5, 8]
+        ]
+        const annouceWinner = () =>{
+            const winMess = document.querySelector("#winnerDiv")
+            if (winnerX == true){
+                winMess.textContent = ("Player X is the winner")
+                
+            } else if (winnerO == true){
+                winMess.textContent = ("Player O is the winner")
+                
+            }else {winMess.textContent = ("draw")}
+        }   
+            conditionsForWin.forEach((winComb) => {
+                const value0 = boardArray[winComb[0]]
+                const value1 = boardArray[winComb[1]]
+                const value2 = boardArray[winComb[2]]
+                if (value0 == "" || value1 == "" || value2 == ""){
+                    return
+                } else if (value0 == "X" && value0 == value1 && value1 == value2){
+                    console.log("holy poggers X won")
+                    winnerX = true;
+           //         gameBoard.stopGame()
+                   annouceWinner()
+           //         winnerX = false;
+                } else if(value0 == "O" && value0 == value1 && value1 == value2){
+                    console.log("O won")
+                    winnerO = true;
+           //         gameBoard.stopGame()
+                    annouceWinner()
+            //        winnerO = false;
+                }else if(!boardArray.includes("") ){
+                    draw = true;
+                    annouceWinner()
+                }
+            })
+        
+    }
+    return{whichSign,  resetRoundC, checkForWin, }
 })();
 
